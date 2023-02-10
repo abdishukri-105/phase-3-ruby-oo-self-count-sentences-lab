86cb5878ca5588a3a38a4275e87be798999c0757
abdishukri <abdishukrimohamed105@gmail.com>
2023-02-10 12:18:41 +0300

    passed all tests

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,26 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self[-1] == "."
   end
 
   def question?
-
+     self[-1] == "?"
   end
 
   def exclamation?
-
+     self[-1] == "!"
   end
 
+  # here i called the self method with split and passed the regex to check
+  # for panctuation mark the plus indicates that the accourance of it one or more times
+  # The reject method returns a new array with the elements that match the given block condition removed.
+  # notice i used shorthand instead of of block
+  # lastly the count method returns the number of elems in the array
   def count_sentences
+    self.split(/[.?!]+/).reject(&:empty?).count
 
   end
 end
+
+binding.pry
